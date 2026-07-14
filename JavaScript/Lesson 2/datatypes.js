25d06da5ed279e0d478702bd05965946eb39dd84
Eman Shahid <139706459+Syeda-Eman@users.noreply.github.com>
2026-07-14 14:43:09 +0300

    Update and rename test.js to datatypes.js

---
+++ JavaScript/Lesson 2/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3
+    +
+    3) //code readability should be high
+
+console.log("Eman")
+
+let name = "Eman"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//number
+// bigint
+//string
+//boolean
+//null
+//symbol
+
+//object
+
+console.log(typeof"Eman"); //string
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
