d23a15f80658ad00fa9a1a8b10f25ee5bca5e777
Eman Shahid <139706459+Syeda-Eman@users.noreply.github.com>
2026-07-16 21:13:25 +0300

    Add datatypes summary in datatypes_summary.js
    Added a summary of JavaScript data types, including examples of primitive and reference types.

---
+++ JavaScript/Lesson 5/datatypes_summary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+
+const outsideTemp = null
+let userEmail; 
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 1234456645322123455678n
+
+
+//Reference (Non Primitive)
+
+// Array, Objects, Functions, 
+
+const cities = ["Riyadh", "Yanbu", "Jeddah", "Jubail"];
+let myObj ={
+    name: "Eman",
+    age: 16,
+}
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
+
+
