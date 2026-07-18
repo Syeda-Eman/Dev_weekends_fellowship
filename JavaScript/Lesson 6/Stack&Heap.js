1addf66c6c6bb182852db8b823c8a3c0d3ff202d
Eman Shahid <139706459+Syeda-Eman@users.noreply.github.com>
2026-07-18 12:50:17 +0300

    Update and rename test.js to Stack&Heap.js

---
+++ JavaScript/Lesson 6/Stack&Heap.js
@@ -0,0 +1,19 @@
+// Stack (Primitive), Heap(Non-Primitive)
+ let myYoutubename = "emanshahidorg"
+
+ let anothername = myYoutubename
+anothername = "syedaeman"
+
+ console.log(myYoutubename);
+ console.log(anothername);
+
+ let userOne = {
+    email: "user@google.com",
+    upi: "user@123"
+ }
+ let userTwo = userOne
+
+ userTwo.email = "eman@gmail.com"
+
+ console.log(userOne.email);
+ console.log(userTwo.email);
