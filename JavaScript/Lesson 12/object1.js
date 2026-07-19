7e84f961e9f48517a643899c31257758e3cd077e
Eman Shahid <139706459+Syeda-Eman@users.noreply.github.com>
2026-07-19 14:21:15 +0300

    Update JsUser object with new user details

--- JavaScript/Lesson 12/object1.js
+++ JavaScript/Lesson 12/object1.js
@@ -7,12 +7,12 @@ const mySym = Symbol("key1")
 
 
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
+    name: "Eman",
+    "full name": "Eman Shahid",
     [mySym]: "mykey1",
-    age: 18,
-    location: "Jaipur",
-    email: "hitesh@google.com",
+    age: 16,
+    location: "Riyadh",
+    email: "eman@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
@@ -22,9 +22,9 @@ const JsUser = {
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
-JsUser.email = "hitesh@chatgpt.com"
+JsUser.email = "eman@chatgpt.com"
 // Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+JsUser.email = "eman@microsoft.com"
 // console.log(JsUser);
 
 JsUser.greeting = function(){
